08d95a2a48cba496d999fe0569c4eb861910e92b
yungen-lu <70182238+yungen-lu@users.noreply.github.com>
2022-12-26 00:27:05 +0800

    fix: change error message format

--- internal/events/message.go
+++ internal/events/message.go
@@ -81,42 +81,42 @@ func (c *Controller) handleText(message *linebot.TextMessage, replyToken string,
 		var opts configOpts
 		err = Parse("config", &opts, args)
 		if err != nil {
-			return fmt.Errorf("info opts: %s", err.Error())
+			return err
 		}
 		return u.FSM.Fire(ConfigEvent, replyToken, opts.UserName, opts.PassWord, opts.AdminToken)
 	case "list":
 		var opts listOpts
 		err = Parse("list", &opts, args)
 		if err != nil {
-			return fmt.Errorf("config opts: %s", err.Error())
+			return err
 		}
 		return u.FSM.Fire(ListEvent, replyToken, opts.All, opts.Namespace)
 	case "info":
 		var opts infoOpts
 		err = Parse("info", &opts, args)
 		if err != nil {
-			return fmt.Errorf("info opts: %s", err.Error())
+			return err
 		}
 		return u.FSM.Fire(InfoEvent, replyToken, opts.DBName, opts.Namespace)
 	case "stop":
 		var opts stopOpts
 		err = Parse("stop", &opts, args)
 		if err != nil {
-			return fmt.Errorf("stop opts: %s", err.Error())
+			return err
 		}
 		return u.FSM.Fire(StopEvent, replyToken, opts.DBName, opts.Namespace)
 	case "create":
 		var opts createOpts
 		err = Parse("create", &opts, args)
 		if err != nil {
-			return fmt.Errorf("create opts: %s", err.Error())
+			return err
 		}
 		return u.FSM.Fire(CreateEvent, replyToken, opts.DBName, opts.DBType, opts.Namespace)
 	case "myinfo":
 		var opts userInfoOpts
 		err = Parse("myinfo", &opts, args)
 		if err != nil {
-			return fmt.Errorf("create opts: %s", err.Error())
+			return err
 		}
 		return u.FSM.Fire(UserInfoEvent, replyToken, opts.All)
 	case "back":
@@ -132,11 +132,11 @@ func Parse(name string, data interface{}, args []string) error {
 	parser := flags.NewNamedParser(name, flags.Default)
 	_, err := parser.AddGroup("Application Options", "", data)
 	if err != nil {
-		return fmt.Errorf("config opts: %s", err.Error())
+		return err
 	}
 	_, err = parser.ParseArgs(args)
 	if err != nil {
-		return fmt.Errorf("config opts: %s", err.Error())
+		return err
 	}
 	return nil
 }
